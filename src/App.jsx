75fdd00dd97ae6bbebd9a1b807dbb5e380c63270
CodeSandbox <support@codesandbox.io>
2025-05-14 04:48:07 +0000

    Sandbox snapshot commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  function handleInput() {
+    //please put your logic here
+  }
+
+  return (
+    <div className="App">
+      <div>
+        Number: <input />
+        <button>Submit</button>
+      </div>
+      <div>Result: 0</div>
+    </div>
+  );
+}
